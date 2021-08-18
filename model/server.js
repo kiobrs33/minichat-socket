1d75f6c802725fa2b13f866891645ec7d7859d53
Rene <rene.lozano@tecsup.edu.pe>
2021-08-18 11:17:01 -0500

    first commit

--- model/server.js
+++ model/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 class Server {
 
@@ -20,6 +21,7 @@ class Server {
 
     middlewares() {
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ));
+        this.app.use( cors() );
     }
 
     configSockets(){
